987fda5268f2b2b880337b5f7bf518ecde893487
Xiao Xiao <xiaosquared@gmail.com>
2015-08-24 17:12:03 -0400

    calibrated for office piano + remote control to play

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var staticRoot = path.join(__dirname+'/public/');
 
 //Expose static content
@@ -8,11 +10,23 @@ app.use(express.static('public'));
 
 //Routes
 app.get('/mf', function(req, res){
-    res.sendFile(staticRoot+"index.html");
+    res.sendFile(staticRoot+"fugue.html");
 });
 
-//Server
-var server = app.listen(3000, function(){
+app.get('/remote', function(req, res){
+    res.sendFile(staticRoot+"remote.html");
+});
+
+//WS Server
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('control', function (msg) {
+    console.log(msg);
+    io.emit('control', msg);
+  });
+});
+
+server.listen(3000,function(){
     var port = server.address().port;
     var host = server.address().address;
     (host == '::') ? host = 'localhost' : host = host;
